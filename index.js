26cebce7e65b9c9ec39c9c34fb2c642927001058
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-21 16:18:00 -0300

    listaDeCompras

---
+++ index.js
@@ -0,0 +1,25 @@
+// const emojis = ['🐥','🐯','🐼']
+// console.log(emojis.includes('🐴'))
+
+const addItemBtn = document.getElementById('add-item-btn')
+const itemInput = document.getElementById('item-input')
+const list = document.getElementById('list')
+
+const shoppingList = []
+    
+addItemBtn.addEventListener('click', function(){
+        shoppingList.push(itemInput.value)
+        render()        
+        itemInput.value = '' 
+})
+
+function render(){    
+    let html = ''
+    for (let item of shoppingList){
+        html+= `<li class="list-item">${item}</li>`
+    }
+    list.innerHTML = html
+}
+
+render()
+
